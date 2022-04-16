13fc8efd2ab43feac0905e0ca1429f5e1f9dfb53
aagraston <aagraston@gmail.com>
2022-04-15 18:19:02 -0700

    creating a server with require('http')

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 require('dotenv').config()
 
+const http = require('http')
+
+const port = process.env.PORT
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello, world!</h1>')
+})
+
+server.listen(port, () => {
+  console.log(`server running at port ${port}`)
+})
+
+
 
 //code
 process.env.USER_ID
